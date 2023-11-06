08dda96fd87697a5a2f1fe57e5c8e5171685786d
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-06 00:04:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
 #Importing required packages
 import streamlit as st
 import openai
+import promptlayer
+import uuid
 
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 #MODEL = "gpt-3"
 #MODEL = "gpt-3.5-turbo"
 #MODEL = "gpt-3.5-turbo-0613"
 #MODEL = "gpt-3.5-turbo-16k"
-#MODEL = "gpt-3.5-turbo-16k-0613"
+MODEL = "gpt-3.5-turbo-16k-0613"
 #MODEL = "gpt-4"
-MODEL = "gpt-4-0613"
+#MODEL = "gpt-4-0613"
 #MODEL = "gpt-4-32k-0613"
 
-st.set_page_config(page_title="Data Quality Bot")
-st.sidebar.title("Learn about Data Quality")
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+    
+st.set_page_config(page_title="Learn About Data Quality")
+st.sidebar.title("Learn Data Quality")
 st.sidebar.divider()
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-st.sidebar.markdown("Current Version: 0.2.0")
-st.sidebar.markdown("Using GPT-4 API")
+st.sidebar.markdown("Current Version: 1.0.0")
+st.sidebar.markdown("Using gpt-3.5-turbo-16k-0613 API")
+st.sidebar.markdown(st.session_state.session_id)
 st.sidebar.divider()
+# Check if the user has provided an API key, otherwise default to the secret
+user_openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", placeholder="sk-...", type="password")
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = MODEL
+
+if user_openai_api_key:
+    # If the user has provided an API key, use it
+    # Swap out openai for promptlayer
+    promptlayer.api_key = st.secrets["PROMPTLAYER"]
+    openai = promptlayer.openai
+    openai.api_key = user_openai_api_key
+else:
+    st.warning("Please enter your OpenAI API key", icon="⚠️")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -55,23 +71,25 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+if user_openai_api_key:
+    if prompt := st.chat_input("How can I help with Wardley Mapping?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+    
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+                pl_tags=["learnwardleymapping", st.session_state.session_id],
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
